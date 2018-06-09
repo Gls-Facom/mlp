66cb5cdbba8d6428f37f050038928e18023e1f4a
caiosuzuki <cpolidoro@gmail.com>
2018-06-08 23:57:28 -0400

    funcoes de ativacao quase terminadas

--- network.py
+++ network.py
@@ -9,7 +9,7 @@ from dataHandler import DataHandler
 
 class Network(object):
 
-    def __init__(self, sizes, dataHandler, layers):
+    def __init__(self, sizes, dataHandler):
         self.num_layers = len(sizes)
         self.sizes = sizes
         self.dataHandler = dataHandler
